6921541322579a90b97377503af384ce8936f186
Linyuanjia <874777007@qq.com>
2019-11-13 09:45:46 +0800

    小程序第一天进度

---
+++ 02_demo/pages/index/index.js
@@ -0,0 +1,96 @@
+// pages/index/index.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 轮播导航栏的数据
+    navList:{
+      listData1: {
+        1:{url:"../../imgs/logo.png",name:"LOGO设计"},
+        2:{url:"../../imgs/pinpai.png",name:"品牌VI设计"},
+        3:{url:"../../imgs/huace.png",name:"画册设计"},
+        4:{url:"../../imgs/wenhua.png",name:"企业文化墙"},
+        5:{url:"../../imgs/baozhuang.png",name:"包装设计"},
+        6:{url:"../../imgs/quanan.png",name:"品牌全案"},
+        7:{url:"../../imgs/zhuanmai.png",name:"专卖店设计"},
+        8:{url:"../../imgs/more.png",name:"更多"}
+      },
+      listData2:{
+        1:{url:"../../imgs/logo.png",name:"LOGO设计"},
+        2:{url:"../../imgs/pinpai.png",name:"品牌VI设计"},
+        3:{url:"../../imgs/huace.png",name:"画册设计"},
+        4:{url:"../../imgs/wenhua.png",name:"企业文化墙"},
+        5:{url:"../../imgs/baozhuang.png",name:"包装设计"},
+        6:{url:"../../imgs/quanan.png",name:"品牌全案"},
+        7:{url:"../../imgs/zhuanmai.png",name:"专卖店设计"},
+        8:{url:"../../imgs/more.png",name:"更多"}
+      }
+    },
+
+    // 中部通知content的数据
+    notifyList: {
+      abc:["1333****333用户购买logo一件.....","1333****333升级为 VIP 2级....."],
+      nba:["baseline：项目的第一行文字的基线对齐","justify-content属性定义项目在主轴上的对齐方式"],
+      cba:["space-between:两端对齐","space-around：每个项目两侧的间隔相等"],
+      mba:["align-content属性定义了多根轴线的对齐方式","如果项目只有一根轴线，该属性不起作用"]
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
