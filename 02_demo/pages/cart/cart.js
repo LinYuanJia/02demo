bc48b3591876c855f7c1421954ec66ded253fcfb
Linyuanjia <874777007@qq.com>
2019-11-14 18:28:15 +0800

    第三天进度

--- 02_demo/pages/cart/cart.js
+++ 02_demo/pages/cart/cart.js
@@ -5,8 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 推荐展示数据
+    showData:{
+      1:{url:"../../imgs/goods/01.jpg",title:"古典风格 画册设计",name:"设计师老王"},
+      2:{url:"../../imgs/goods/02.jpg",title:"古典风格 画册设计",name:"设计师tony"},
+      3:{url:"../../imgs/goods/03.jpg",title:"古典风格 画册设计",name:"设计师小张"},
+      4:{url:"../../imgs/goods/04.jpg",title:"古典风格 画册设计",name:"设计师UZI"},
+      5:{url:"../../imgs/goods/05.jpg",title:"古典风格 画册设计",name:"设计师菜鸟"},
+      6:{url:"../../imgs/goods/got08.jpg",title:"古典风格 画册设计",name:"设计师哈哈哈"}
+    }
 
   },
+
+  /* ---------------事件监听函数------------- */
+
+  checkboxChange(even){
+    console.log(even)
+  },
+
+
+
+
+  /* ---------------生命周期函数----------------- */
 
   /**
    * 生命周期函数--监听页面加载
--- 02_demo/pages/profile/profile.js
+++ 02_demo/pages/profile/profile.js
@@ -5,6 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
+     /* 导航栏图标数据 */
+     navData:{
+       1:{url:"/imgs/profile/dingdan.png",title:"全部订单"},
+       2:{url:"/imgs/profile/daifu.png",title:"待付款"},
+       3:{url:"/imgs/profile/keyong.png",title:"可使用"},
+       4:{url:"/imgs/profile/shouhou.png",title:"退款/售后"}
+     },
+     /* 详细列表信息数据 */
+     listData:{
+       1:{url:"/imgs/profile/fenxiao.png",name:"分销商"},
+       2:{url:"/imgs/profile/youhui.png",name:"优惠券"},
+       3:{url:"/imgs/profile/fapiao.png",name:"我的发票"},
+       4:{url:"/imgs/profile/shoucang.png",name:"我的收藏"},
+       5:{url:"/imgs/profile/fankui.png",name:"意见反馈"}
+     }
 
   },
 
